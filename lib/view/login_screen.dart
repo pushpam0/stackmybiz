6b131c6a7d5fe996d26ce82702ba9c8c7b9bcba1
Pushpam Kumar <pushpam@quikieapps.com>
2022-01-26 15:36:53 +0530

    login registration design

---
+++ lib/view/login_screen.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: LayoutBuilder(builder: (context, constraint) {
+          return Align(
+            alignment: Alignment.center,
+            child: SizedBox(
+              height: constraint.maxHeight,
+              child: Column(
+                children: [
+                  Container(
+                    height: constraint.maxHeight * 0.2,
+                    child: Center(
+                      child: Image.asset("assets/stack.png"),
+                    ),
+                  ),
+                  Column(
+                    children: const [
+                      Text(
+                        "We Stack Your Idea Into Successful Biz",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      // ignore: prefer_const_constructors
+                      Text(
+                        "Let's take a successful stacking journey together.",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.grey),
+                      )
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                    height: constraint.maxHeight * 0.5,
+                    //  color: Colors.red[300],
+                    child: Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Email Id",
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Password",
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Align(
+                            alignment: Alignment.topRight,
+                            child: Text(
+                              "Forgot Password?",
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          ),
+                        ),
+                        MaterialButton(
+                          onPressed: () {},
+                          color: Colors.blue,
+                          child: const Text("Sign In"),
+                          padding: EdgeInsets.all(10),
+                          minWidth: MediaQuery.of(context).size.width * 0.4,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        MaterialButton(
+                          onPressed: () {},
+                          color: Colors.white,
+                          child: const Text("Sign In With Google"),
+                          padding: EdgeInsets.all(10),
+                          minWidth: MediaQuery.of(context).size.width * 0.4,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Don't have an account?"),
+                            Text(
+                              "Register",
+                              style: TextStyle(color: Colors.green),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
